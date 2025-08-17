8fd58d514a5476a58dc00f850caf518e1b3b3a4f
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-08-17 16:27:00 +0900

    fix: oauth 사용자 용 더미 비밀번호 설정

--- src/main/java/com/sejong/userservice/application/common/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/sejong/userservice/application/common/security/oauth/CustomOAuth2UserService.java
@@ -44,6 +44,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                     .username(username)
                     .nickname(oAuth2Response.getNickname())
                     .email(oAuth2Response.getEmail())
+                    .encryptPassword("OAUTH2_USER") // OAuth2 사용자용 더미 비밀번호
                     .role(UserRole.OUTSIDER)
                     .profileImageUrl(oAuth2Response.getAvatarUrl())
                     .build();
