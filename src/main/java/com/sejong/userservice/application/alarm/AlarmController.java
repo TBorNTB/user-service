0ab9107c0854cc1e714feff12f1c8705888e1b86
PlusUltraCode <kkd06144@naver.com>
2025-10-24 10:52:28 +0900

    feat: 알람 서비스 및 컨트롤러 구현 - AlarmService를 통한 알람 등록/조회 비즈니스 로직 구현 - AlarmController API 추가 (GET /alarms, PATCH /alarms/{id}/read) - AlarmDto 정의 및 변환 메서드 구성

--- src/main/java/com/sejong/userservice/application/alarm/AlarmController.java
+++ src/main/java/com/sejong/userservice/application/alarm/AlarmController.java
@@ -1,4 +1,38 @@
 package com.sejong.userservice.application.alarm;
 
+import com.sejong.userservice.application.common.security.UserContext;
+import com.sejong.userservice.core.alarm.Alarm;
+import com.sejong.userservice.core.alarm.AlarmType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/alarm")
 public class AlarmController {
+    private final AlarmService alarmService;
+
+    //토큰에서 정보 꺼내야 된다.
+    @GetMapping("/received")
+    public ResponseEntity<List<Alarm>> getAllAlarm(
+            @RequestParam("username")String username,
+            @RequestParam("alarmType") AlarmType alarmType
+    ) {
+//        UserContext currentUser = getCurrentUser();
+        List<Alarm> alarms = alarmService.findAll(username,alarmType);
+        return ResponseEntity.status(HttpStatus.OK).body(alarms);
+    }
+
+    private UserContext getCurrentUser() {
+        return (UserContext) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+    }
 }
