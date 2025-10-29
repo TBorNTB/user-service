a16970c95225aad14ad11afdf6bc78f4516741ab
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-29 10:07:42 +0900

    feat: 인증 코드 발송

---
+++ src/main/java/com/sejong/userservice/application/user/VerificationService.java
@@ -0,0 +1,20 @@
+package com.sejong.userservice.application.user;
+
+import com.sejong.userservice.application.email.EmailSender;
+import com.sejong.userservice.application.user.dto.VerificationRequest;
+import com.sejong.userservice.infrastructure.redis.CacheService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VerificationService {
+
+    private final EmailSender emailSender;
+    private final CacheService cacheService;
+
+    public void sendVerificationCode(VerificationRequest request) {
+        emailSender.send(request.getEmail(), request.getRandomCode());
+        cacheService.save(request);
+    }
+}
