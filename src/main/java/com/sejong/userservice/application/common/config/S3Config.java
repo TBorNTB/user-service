5bbcf4541dff9d3ef15861eb1292abc9994b0ce0
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-11-07 10:55:53 +0900

    feat: s3 최신 버전에 맞게 코드 수정

--- src/main/java/com/sejong/userservice/application/common/config/S3Config.java
+++ src/main/java/com/sejong/userservice/application/common/config/S3Config.java
@@ -1,10 +1,5 @@
 package com.sejong.userservice.application.common.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,16 +20,6 @@ public class S3Config {
 
     @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
-
-    @Bean
-    public AmazonS3 amazonS3() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
-                .build();
-    }
 
     @Bean
     public S3Client s3Client() {
