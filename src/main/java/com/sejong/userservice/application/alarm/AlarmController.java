8a00d077575c6e708d29fb921937ecfcf4f5dfa4
PlusUltraCode <kkd06144@naver.com>
2025-11-11 20:36:22 +0900

    chore(Alarm) : Alarm api 토큰에서 정보 추출되게 수정

--- src/main/java/com/sejong/userservice/application/alarm/AlarmController.java
+++ src/main/java/com/sejong/userservice/application/alarm/AlarmController.java
@@ -3,9 +3,12 @@ package com.sejong.userservice.application.alarm;
 import com.sejong.userservice.application.common.security.UserContext;
 import com.sejong.userservice.core.alarm.Alarm;
 import com.sejong.userservice.core.alarm.AlarmType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +25,14 @@ public class AlarmController {
 
     //토큰에서 정보 꺼내야 된다.
     @GetMapping("/received")
+    @Operation(summary = "받은 알람 표시 api")
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SENIOR', 'FULL_MEMBER', 'ASSOCIATE_MEMBER')")
     public ResponseEntity<List<Alarm>> getAllAlarm(
-            @RequestParam("username")String username,
             @RequestParam("alarmType") AlarmType alarmType
     ) {
-//        UserContext currentUser = getCurrentUser();
-        List<Alarm> alarms = alarmService.findAll(username,alarmType);
+        UserContext currentUser = getCurrentUser();
+        List<Alarm> alarms = alarmService.findAll(currentUser.getUsername(),alarmType);
         return ResponseEntity.status(HttpStatus.OK).body(alarms);
     }
 
