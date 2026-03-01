0e60bc570e7e764512ad6907906d5dc7e8c5e220
PlusUltraCode <kkd06144@naver.com>
2026-03-01 16:51:47 +0900

    세계시 -> 한국시로 변경

--- src/main/java/com/sejong/userservice/UserServiceApplication.java
+++ src/main/java/com/sejong/userservice/UserServiceApplication.java
@@ -1,14 +1,22 @@
 package com.sejong.userservice;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
